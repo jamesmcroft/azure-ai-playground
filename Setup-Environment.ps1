0bfd3f5df8cbe47b0949d6a994d8cb1219cb8477
James Croft <jamz_c@hotmail.co.uk>
2025-01-27 21:14:32 +0000

    Refactor Setup-Environment.ps1 to remove deprecated parameters and add WhatIf and Force switches for improved deployment control

--- Setup-Environment.ps1
+++ Setup-Environment.ps1
@@ -1,26 +1,11 @@
-<#
-.SYNOPSIS
-    Deploys the infrastructure and applications required to run the solution.
-.PARAMETER DeploymentName
-	The name of the deployment.
-.PARAMETER Location
-    The location of the deployment.
-.PARAMETER SkipInfrastructure
-    Whether to skip the infrastructure deployment. Requires InfrastructureOutputs.json to exist in the infra directory.
-.EXAMPLE
-    .\Setup-Environment.ps1 -DeploymentName 'my-deployment' -Location 'westeurope' -SkipInfrastructure $false
-.NOTES
-    Author: James Croft
-#>
-
 param
 (
     [Parameter(Mandatory = $true)]
     [string]$DeploymentName,
     [Parameter(Mandatory = $true)]
     [string]$Location,
-    [Parameter(Mandatory = $false)]
-    [bool]$SkipInfrastructure
+    [switch]$WhatIf,
+    [switch]$Force
 )
 
 function Set-ConfigurationFileVariable($configurationFile, $variableName, $variableValue) {
@@ -30,27 +15,22 @@ function Set-ConfigurationFileVariable($configurationFile, $variableName, $varia
 
     if (Select-String -Path $configurationFile -Pattern $variableName) {
         (Get-Content $configurationFile) | Foreach-Object {
-            $_ -replace "$variableName = .*", "$variableName = $variableValue"
+            $_ -replace "$variableName=.*", "$variableName=$variableValue"
         } | Set-Content $configurationFile
     }
     else {
-        Add-Content -Path $configurationFile -value "$variableName = $variableValue"
+        Add-Content -Path $configurationFile -value "$variableName=$variableValue"
     }
 }
 
 Write-Host "Starting environment setup..."
 
-if ($SkipInfrastructure -eq $false) {
-    Write-Host "Deploying infrastructure..."
-    $InfrastructureOutputs = (./infra/Deploy-Infrastructure.ps1 `
-            -DeploymentName $DeploymentName `
-            -Location $Location `
-            -ErrorAction Stop)
-}
-else {
-    Write-Host "Skipping infrastructure deployment. Using existing outputs..."
-    $InfrastructureOutputs = Get-Content -Path './infra/InfrastructureOutputs.json' -Raw | ConvertFrom-Json
-}
+Write-Host "Deploying infrastructure..."
+$InfrastructureOutputs = (./infra/Deploy-Infrastructure.ps1 `
+        -DeploymentName $DeploymentName `
+        -Location $Location `
+        -WhatIf:$WhatIf `
+        -Force:$Force)
 
 $ResourceGroupName = $InfrastructureOutputs.resourceGroupInfo.value.name
 $ManagedIdentityClientId = $InfrastructureOutputs.managedIdentityInfo.value.clientId
